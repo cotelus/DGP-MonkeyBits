5e524d65ac5b96a0022186c47c94b93d51850280
Alejandro de la Plata Ramos <superkorlas@gmail.com>
2018-10-09 17:29:35 +0200

    Añadidos cambios par poder utilizar Firebase Authentication.

--- monkeybit/app/src/main/java/com/monkeybit/routability/MainActivity.java
+++ monkeybit/app/src/main/java/com/monkeybit/routability/MainActivity.java
@@ -2,6 +2,7 @@ package com.monkeybit.routability;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
