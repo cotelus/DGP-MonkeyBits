a76d9d17ce3c9cf197f292c067c4fea0dfa09caa
Alejandro de la Plata Ramos <superkorlas@gmail.com>
2018-10-17 21:06:53 +0200

    Se accede al perfil desde barra de menú.

--- monkeybit/app/src/main/java/com/monkeybit/routability/MainActivity.java
+++ monkeybit/app/src/main/java/com/monkeybit/routability/MainActivity.java
@@ -1,21 +1,43 @@
 package com.monkeybit.routability;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     private FirebaseAuth mAuth;
+    public BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
+
+        bottomNavigationView =  findViewById(R.id.bottonNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_profile:
+                OnLookProfile(item.getActionView());
+                break;
+            case R.id.menu_rutas:
+                Toast toast = Toast.makeText(getApplicationContext(), "Se cargarían las rutas.", Toast.LENGTH_SHORT);
+                toast.show();
+                break;
+        }
+        return true;
     }
 
     public void OnLookProfile(android.view.View view) {
