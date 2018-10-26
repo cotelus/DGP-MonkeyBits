198d7bd1d78f3daa6420f6f084543e3624214e74
Alejandro de la Plata Ramos <superkorlas@gmail.com>
2018-10-26 13:19:22 +0200

    Pequeños cambios para la interfaz (no testeados)

--- monkeybit/app/src/main/java/com/monkeybit/routability/MainActivity.java
+++ monkeybit/app/src/main/java/com/monkeybit/routability/MainActivity.java
@@ -3,6 +3,7 @@ package com.monkeybit.routability;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,6 +16,7 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     private FirebaseAuth mAuth;
     public BottomNavigationView bottomNavigationView;
+    public NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,22 +26,24 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         LoadNewFragment(new MenuActivity());
         bottomNavigationView =  findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setActivated(false);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         Fragment selectedFragment = null;
         switch (item.getItemId()) {
-            case R.id.menu_profile:
-                //OnLookProfile(item.getActionView());
-                if (mAuth.getCurrentUser() == null) {
-                    selectedFragment = new AccountActivity();
-                } else {
-                    selectedFragment = new UserProfileActivity();
-                }
-                break;
             case R.id.menu_rutas:
                 selectedFragment = new MenuActivity();
+                break;
+            case R.id.menu_maps:
+                // @TODO: asignar a selectedFragment el Fragmen de mapas
+                // selectedFragment = new MapsActivity();
+                break;
+            case R.id.menu_fav:
+                // @TODO: asignar a selectedFragment el Fragmen de favoritos
+                // selectedFragment = new FavActivity();
                 break;
         }
         if (selectedFragment != null) {
@@ -52,3 +56,12 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, newFragment).commit();
     }
 }
+
+/*
+     if (mAuth.getCurrentUser() == null) {
+                    selectedFragment = new AccountActivity();
+     } else {
+                    selectedFragment = new UserProfileActivity();
+                }
+
+                */
