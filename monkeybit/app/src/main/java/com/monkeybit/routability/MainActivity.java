26161148cff9a5d272e61303fc4072d49ab973f8
Alejandro de la Plata Ramos <superkorlas@gmail.com>
2018-10-30 20:40:35 +0100

    Menu hamburguesa añadido y totalmente funcional.

--- monkeybit/app/src/main/java/com/monkeybit/routability/MainActivity.java
+++ monkeybit/app/src/main/java/com/monkeybit/routability/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -80,6 +81,12 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
             LoadNewFragment(selectedFragment);
         }
         return true;
+    }
+
+    public void OnShowOptionMenu(View view) {
+        if (!(mainDrawerLayout.isDrawerOpen(navigationView))) {
+            mainDrawerLayout.openDrawer(navigationView);
+        }
     }
 
     public void LogOut() {
