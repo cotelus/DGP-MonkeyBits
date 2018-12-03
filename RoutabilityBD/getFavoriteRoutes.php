9aa3b6d53b4db668f5dea9167f7bcbe9e5e7f231
Luis Ángel Cotelo Leyva <cotelus@gmail.com>
2018-12-03 23:28:33 +0100

    Añado archivos api:
    getFavoritePlaces.php, getFavoriteRoutes.php

---
+++ RoutabilityBD/getFavoriteRoutes.php
@@ -0,0 +1,28 @@
+<?PHP
+$hostname="localhost";
+$database="routability";
+$username="luis";
+$password="12345";
+$json=array();
+	if(isset($_GET["Email"])){
+		$Email = $_GET['Email'];
+
+		$connection=mysqli_connect($hostname,$username,$password,$database);
+		
+		$sql="SELECT IdRoute FROM favoriteroutes WHERE Email IN ('".$Email."')";
+		$result=mysqli_query($connection,$sql);
+
+		if($sql){
+			$x = 0;
+			while($reg = mysqli_fetch_array($result)){
+				$json['data'][$x]=$reg;
+				$x++;
+
+			}
+			$json['OPERATION']="GET_FAVORITE_ROUTES";
+			mysqli_close($connection);
+			echo json_encode($json);
+
+		}
+	}
+?>
