7e8060598a55bc050c1bbaa9601c67d55635d3e4
Luis Ángel Cotelo Leyva <cotelus@gmail.com>
2018-12-16 21:49:09 +0100

    funcionalidad añadida motor busqueda

--- API/searchByName.php
+++ API/searchByName.php
@@ -11,55 +11,73 @@ $json2 = array();
 	if(isset($_GET["Search"])){
 		$searchItems = $_GET['Search'];
 
+		$searchAccessibility = NULL;
+
 		if(!isset($_GET["RedMovility"])){
 			$RedMovility = 0;
 		}else{
 			$RedMovility = $_GET["RedMovility"];
+			$searchAccessibility .= "AND RedMovility = {$_GET["RedMovility"]} ";
 		}
 
 		if(!isset($_GET["RedVision"])){
 			$RedVision = 0;
 		}else{
 			$RedVision = $_GET["RedVision"];
+			$searchAccessibility .= "AND RedVision = {$_GET["RedVision"]} ";
 		}
 
 		if(!isset($_GET["ColourBlind"])){
 			$ColourBlind = 0;
 		}else{
 			$ColourBlind = $_GET["ColourBlind"];
+			$searchAccessibility .= "AND ColourBlind = {$_GET["ColourBlind"]} ";
 		}
 
 		if(!isset($_GET["Deaf"])){
 			$Deaf = 0;
 		}else{
 			$Deaf = $_GET["Deaf"];
+			$searchAccessibility .= "AND Deaf = {$_GET["Deaf"]} ";
 		}
 
 		if(!isset($_GET["Foreigner"])){
 			$Foreigner = 0;
 		}else{
 			$Foreigner = $_GET["Foreigner"];
+			$searchAccessibility .= "AND Foreigner = {$_GET["Foreigner"]} ";
 		}
+
 
 
 		$names = array();
 		$names = explode(" ", $searchItems);
 		$numberNames = count($names);
 		$searchString = "(";
+		$searchDescription = "(";
+		$searchLocalitation = "(";
 
 		for ($i = 0; $i < $numberNames; $i++){
 			$searchString .= "Name LIKE '%{$names[$i]}%'";
+			$searchDescription .= "Description LIKE '%{$names[$i]}%'";
+			$searchLocalitation .= "Localitation LIKE '%{$names[$i]}%'";
+
 			if($i != $numberNames-1){
 				$searchString .= " OR ";
+				$searchDescription .= " OR ";
+				$searchLocalitation .= " OR ";
 			}else{
 				$searchString .= ")";
+				$searchDescription .= ")";
+				$searchLocalitation .= ")";
 			}
 		}
 
 
 		$connection=mysqli_connect($hostname,$username,$password,$database);
 		
-		$sql1="SELECT * FROM place WHERE {$searchString} AND RedMovility = {$RedMovility} AND RedVision = {$RedVision} AND ColourBlind = {$ColourBlind} AND Deaf = {$Deaf} AND Foreigner = {$Foreigner}";
+		//$sql1="SELECT * FROM place WHERE {$searchString}/* OR {$searchDescription} OR {$searchLocalitation} */AND RedMovility = {$RedMovility} AND RedVision = {$RedVision} AND ColourBlind = {$ColourBlind} AND Deaf = {$Deaf} AND Foreigner = {$Foreigner}";
+		$sql1="SELECT * FROM place WHERE {$searchString} OR {$searchDescription} OR {$searchLocalitation} {$searchAccessibility}";
 		$result=mysqli_query($connection,$sql1);
 
 		if($sql1){
@@ -86,7 +104,8 @@ $json2 = array();
 
 			}
 
-		$sql2 = "SELECT * FROM route WHERE {$searchString}";
+		
+		$sql2 = "SELECT * FROM route WHERE {$searchString} OR {$searchDescription}";
 		$result=mysqli_query($connection,$sql2);
 
 		if($sql2){
@@ -99,7 +118,7 @@ $json2 = array();
 				$jsonTuple2['Email'] = $reg['Email'];
 				$jsonTuple2['MadeBy'] = $reg['MadeBy'];
 				$jsonTuple2['Name'] = $reg['Name'];
-				$jsonTuple2['Description'] = $reg['Image'];
+				$jsonTuple2['Description'] = $reg['Description'];
 
 				$json['SEARCH_BY_NAME_ROUTES'][$x]=$jsonTuple2;
 				$x++;
