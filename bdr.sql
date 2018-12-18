a94e93c0947a82605e07a6d63456aef91376f43f
Ramón Jesús <ramonjtorres1997@gmail.com>
2018-12-18 14:30:52 +0100

    Cambios en BBDD
    Se ha cambiado las tildes

--- bdr.sql
+++ bdr.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.7.9
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-12-2018 a las 17:44:58
--- Versión del servidor: 10.1.36-MariaDB
--- Versión de PHP: 7.2.11
+-- Tiempo de generación: 18-12-2018 a las 14:28:12
+-- Versión del servidor: 10.1.31-MariaDB
+-- Versión de PHP: 7.2.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -151,15 +151,15 @@ CREATE TABLE `place` (
 INSERT INTO `place` (`IdPlace`, `Email`, `MadeBy`, `Name`, `Description`, `Localitation`, `Image`, `ImageDescription`, `RedMovility`, `RedVision`, `ColourBlind`, `Deaf`, `Foreigner`) VALUES
 (1, 'admin1@gmail.com', 'usuario1@gmail.com', 'Lugar bonito', 'fasefas', 'asefas', 'afsesa', '', 0, 0, 0, 0, 0),
 (2, 'admin2@hotmail.com', 'usuario1@gmail.com', 'Cansado del nombre asfas', 'asfeasef', 'afsesa', 'afsef', '', 0, 0, 0, 0, 0),
-(16, 'dgp.monkeybits@gmail.com', NULL, 'Parque Almunia', 'Delimitado por los viales Avda. de Andalucía, Calle Periodista Eugenio Selles y Calle Periodista José Mezcua Ruiz se extiende este bonito parque.\r\n\r\nTiene una distribución triangular rodeado por una valla y muro, además de existir distintas pantallas vegetales en cada uno de los lados: ciprés, pruno, espino de fuego y laurel. Se desarrolla a partir de diversos paseos que se complementan y relacionan a lo largo de todo su recorrido, delimitando numerosas zonas de estancia con elementos característicos como el quiosco de música o la fuente con pérgola de glicinas en su parte posterior. Uno de los elementos principales del parque es la acequia, atravesada por puentecillos de piedra que dan paso a cuadros de aromáticas, hacia un lado, y a una paseo de arces, al otro.\r\n\r\nDistrito: Chana\r\nSuperficie: 32.142 metros cuadrados\r\nTipo de parque: Cerrado', 'Delimitado por los viales Avda. de Andalucía. Granada.', 'http://www.besarsengranada.es/wp-content/uploads/2015/05/besarse-en-granada-flores-parque-almunia-5_21390.jpg', '', 0, 0, 0, 0, 0),
-(17, '', NULL, 'Parque Federico García Lorca', 'El Parque García Lorca, con cuatro entradas , se desarrolla alrededor de la Huerta de San Vicente, en los terrenos que antiguamente pertenecían a la familia de Federico García Lorca. El parque está constituido por diversos paseos , destacando dos avenidas: el paseo de la alameda , el cual se desarrolla paralelamente al río y se dirige al estanque , y el paseo de tilos, desde la entrada principal hasta las zonas de servicios Delimitados por estos paseos y caminos, existen diversos sectores que terminan de configurar este parque: los jardines neoplasticistas, las acequias, la fuente cibernética, el bosque de ribera, la rosaleda y las huertas.\r\n\r\nDistrito: Ronda\r\nSuperficie: 70.000 metros cuadrados\r\nTipo de parque: Cerrado', 'Entre las calles Arabial , Virgen Blanca y el Camino de Purchil.', 'https://www.conmishijos.com/assets/planes/4000/4120-parque-federico-garcia-lorca-granada.jpg', '', 0, 0, 0, 0, 0),
+(16, 'dgp.monkeybits@gmail.com', NULL, 'Parque Almunia', 'Delimitado por los viales Avda. de Andalucia, Calle Periodista Eugenio Selles y Calle Periodista Jose Mezcua Ruiz se extiende este bonito parque.\r\n\r\nTiene una distribucion triangular rodeado por una valla y muro, ademas de existir distintas pantallas vegetales en cada uno de los lados: cipres, pruno, espino de fuego y laurel. Se desarrolla a partir de diversos paseos que se complementan y relacionan a lo largo de todo su recorrido, delimitando numerosas zonas de estancia con elementos caracteristicos como el quiosco de musica o la fuente con pergola de glicinas en su parte posterior. Uno de los elementos principales del parque es la acequia, atravesada por puentecillos de piedra que dan paso a cuadros de aromaticas, hacia un lado, y a una paseo de arces, al otro.\r\n\r\nDistrito: Chana\r\nSuperficie: 32.142 metros cuadrados\r\nTipo de parque: Cerrado', 'Delimitado por los viales Avda. de Andalucia. Granada.', 'http://www.besarsengranada.es/wp-content/uploads/2015/05/besarse-en-granada-flores-parque-almunia-5_21390.jpg', '', 0, 0, 0, 0, 0),
+(17, '', NULL, 'Parque Federico Garcia Lorca', 'El Parque Garcia Lorca, con cuatro entradas , se desarrolla alrededor de la Huerta de San Vicente, en los terrenos que antiguamente pertenecian a la familia de Federico Garcia Lorca. El parque esta constituido por diversos paseos , destacando dos avenidas: el paseo de la alameda , el cual se desarrolla paralelamente al rio y se dirige al estanque , y el paseo de tilos, desde la entrada principal hasta las zonas de servicios Delimitados por estos paseos y caminos, existen diversos sectores que terminan de configurar este parque: los jardines neoplasticistas, las acequias, la fuente cibernética, el bosque de ribera, la rosaleda y las huertas.\r\n\r\nDistrito: Ronda\r\nSuperficie: 70.000 metros cuadrados\r\nTipo de parque: Cerrado', 'Entre las calles Arabial , Virgen Blanca y el Camino de Purchil.', 'https://www.conmishijos.com/assets/planes/4000/4120-parque-federico-garcia-lorca-granada.jpg', '', 0, 0, 0, 0, 0),
 (18, '', 'usuariobloqueado@gmail.com', 'fasef', 'fasef', '', 'asfes', '', 0, 0, 0, 0, 0),
-(19, 'dgp.monkeybits@gmail.com', NULL, 'Jardines del Triunfo', 'Se extiende este bonito parque, que tiene una distribución triangular rodeado por una valla y muro, además de existir distintas pantallas vegetales en cada uno de los lados: ciprés, pruno, espino de fuego y laurel. Se desarrolla a partir de diversos paseos que se complementan y relacionan a lo largo de todo su recorrido, delimitando numerosas zonas de estancia con elementos característicos como el quiosco de música o la fuente con pérgola de glicinas en su parte posterior. Uno de los elementos principales del parque es la acequia, atravesada por puentecillos de piedra que dan paso a cuadros de aromáticas, hacia un lado, y a una paseo de arces, al otro.\r\n\r\nDistrito: Chana\r\nSuperficie: 32.142 metros cuadrados\r\nTipo de parque: Cerrado', 'Avda. de Andalucía, c/Periodista Eugenio Selles, c/Periodista José Mezcua Ruiz.', 'http://2.bp.blogspot.com/_vf7VA2YFrwU/TP_aQ1j9O1I/AAAAAAAAEDM/9BAgOnll5js/s400/8%2Bde%2Bdiciembre%2B1.jpg', '', 1, 0, 0, 0, 1),
-(20, 'dgp.monkeybits@gmail.com', NULL, 'Mirador de San Nicolás', 'El Mirador de San Nicolás es quizás el más famoso de Granada. Desde que Bill Clinton lo visitara en 1997 y dijera que vio “la puesta de sol más bonita del mundo”, se ha convertido en visita obligada para los turistas.\r\n\r\nLa belleza de este mirador es innegable: la Alhambra y el Generalife frente a frente, la ciudad a sus pies y Sierra Nevada detrás.\r\n\r\nEl encanto de su entorno, en pleno barrio del Albaycín, con sus calles empedradas, sus aljibes, sus bares de tapas y sus gentes, añade sabor a la estampa. \r\n\r\nEstá siempre animado, con foráneos y locales. Abundan los vendedores de artesanía y grupos que con sus guitarras improvisan toques flamencos.', 'Albaicín, Granada', 'http://nisnomad.com/wp-content/uploads/2016/08/IMG_20160812_081240.jpg', '', 1, 0, 0, 0, 1),
-(21, 'dgp.monkeybits@gmail.com', NULL, 'Alhambra', 'Es una ciudad palatina andalusí situada en Granada, en comunidad autónoma de Andalucía, España. Consiste en un conjunto de palacios, jardines y fortaleza que albergaba una verdadera ciudadela dentro de la propia ciudad de Granada, que servía como alojamiento al monarca y a la corte del Reino nazarí de Granada. Su verdadero atractivo, como en otras obras musulmanas de la época, no solo radica en los interiores, cuya decoración está entre las cumbres del arte andalusí, sino también en su localización y adaptación, generando un paisaje nuevo pero totalmente integrado con la naturaleza preexistente.', 'Calle Real de la Alhambra', 'https://cdn.getyourguide.com/img/tour_img-989388-148.jpg', '', 1, 1, 0, 0, 1),
-(22, 'dgp.monkeybits@gmail.com', NULL, 'Generalife', 'Es la villa con jardines habitada por los reyes musulmanes de Granada como lugar de descanso, situado en la ciudad española de Granada, en Andalucía. Fue concebida como villa rural, donde jardines ornamentales, huertos y arquitectura se integraban, en las cercanías de la Alhambra. El origen del nombre está discutido. Algunos abogan por Yannat al-Arif como «huerta del arquitecto», o «jardín del arquitecto» aunque pudo significar «el más excelso jardín». Ese huerto real era común en las cortes hispano-árabes y es fruto de las reformas y añadidos que le aportaron los diferentes sultanes.', ' Exterior de las murallas de la Alhambra, al este, en la ladera del Cerro del Sol.', 'https://media-cdn.tripadvisor.com/media/photo-s/11/7e/aa/fb/jardines-del-generalife.jpg', '', 1, 1, 0, 0, 1),
-(23, 'dgp.monkeybits@gmail.com', NULL, 'Patio de los leones', 'El Patio de los Leones es el centro del Palacio que lleva este nombre. Es diferente al del Palacio de Comares, debido al eje del jardín anterior, del siglo XIII, sobre el que se diseña y a que el baño de Comares tiene un eje Norte-Sur y el palacio de los Leones tiene un eje Este-Oeste.', 'Calle Real de la Alhambra.', 'http://cadenaser00.epimg.net/emisora/imagenes/2016/08/30/radio_granada/1472561813_422996_1472561946_noticia_normal.jpg', '', 1, 1, 0, 0, 1),
-(34, 'dgp.monkeybits@gmail.com', NULL, 'Palacio de Carlos V', 'El palacio de Carlos V es una construcción renacentista situada en la colina de la Alhambra de la ciudad española de Granada, en Andalucía. Desde 1958, es sede del Museo de Bellas Artes de Granada y, desde 1994, también es sede del Museo de la Alhambra.', 'calle de la Alhambra', 'https://www.101viajes.com/sites/default/files/palacio-carlos-v.jpg', '', 1, 1, 0, 0, 1);
+(19, 'dgp.monkeybits@gmail.com', NULL, 'Jardines del Triunfo', 'Se extiende este bonito parque, que tiene una distribucion triangular rodeado por una valla y muro, ademas de existir distintas pantallas vegetales en cada uno de los lados: cipres, pruno, espino de fuego y laurel. Se desarrolla a partir de diversos paseos que se complementan y relacionan a lo largo de todo su recorrido, delimitando numerosas zonas de estancia con elementos caracteristicos como el quiosco de musica o la fuente con pergola de glicinas en su parte posterior. Uno de los elementos principales del parque es la acequia, atravesada por puentecillos de piedra que dan paso a cuadros de aromaticas, hacia un lado, y a una paseo de arces, al otro.\r\n\r\nDistrito: Chana\r\nSuperficie: 32.142 metros cuadrados\r\nTipo de parque: Cerrado', 'Avda. de Andalucia, c/Periodista Eugenio Selles, c/Periodista Jose Mezcua Ruiz.', 'http://2.bp.blogspot.com/_vf7VA2YFrwU/TP_aQ1j9O1I/AAAAAAAAEDM/9BAgOnll5js/s400/8%2Bde%2Bdiciembre%2B1.jpg', '', 1, 0, 0, 0, 1),
+(20, 'dgp.monkeybits@gmail.com', NULL, 'Mirador de San Nicolas', 'El Mirador de San Nicolas es quizas el mas famoso de Granada. Desde que Bill Clinton lo visitara en 1997 y dijera que vio “la puesta de sol más bonita del mundo”, se ha convertido en visita obligada para los turistas.\r\n\r\nLa belleza de este mirador es innegable: la Alhambra y el Generalife frente a frente, la ciudad a sus pies y Sierra Nevada detras.\r\n\r\nEl encanto de su entorno, en pleno barrio del Albaycin, con sus calles empedradas, sus aljibes, sus bares de tapas y sus gentes, aniade sabor a la estampa. \r\n\r\nEsta siempre animado, con foraneos y locales. Abundan los vendedores de artesania y grupos que con sus guitarras improvisan toques flamencos.', 'Albaicin, Granada', 'http://nisnomad.com/wp-content/uploads/2016/08/IMG_20160812_081240.jpg', '', 1, 0, 0, 0, 1),
+(21, 'dgp.monkeybits@gmail.com', NULL, 'Alhambra', 'Es una ciudad palatina andalusi situada en Granada, en comunidad autonoma de Andalucia, Espania. Consiste en un conjunto de palacios, jardines y fortaleza que albergaba una verdadera ciudadela dentro de la propia ciudad de Granada, que servia como alojamiento al monarca y a la corte del Reino nazari de Granada. Su verdadero atractivo, como en otras obras musulmanas de la epoca, no solo radica en los interiores, cuya decoracion esta entre las cumbres del arte andalusi, sino tambien en su localizacion y adaptacion, generando un paisaje nuevo pero totalmente integrado con la naturaleza preexistente.', 'Calle Real de la Alhambra', 'https://cdn.getyourguide.com/img/tour_img-989388-148.jpg', '', 1, 1, 0, 0, 1),
+(22, 'dgp.monkeybits@gmail.com', NULL, 'Generalife', 'Es la villa con jardines habitada por los reyes musulmanes de Granada como lugar de descanso, situado en la ciudad espaniola de Granada, en Andalucia. Fue concebida como villa rural, donde jardines ornamentales, huertos y arquitectura se integraban, en las cercanias de la Alhambra. El origen del nombre esta discutido. Algunos abogan por Yannat al-Arif como «huerta del arquitecto», o «jardin del arquitecto» aunque pudo significar «el mas excelso jardin». Ese huerto real era comun en las cortes hispano-arabes y es fruto de las reformas y aniadidos que le aportaron los diferentes sultanes.', ' Exterior de las murallas de la Alhambra, al este, en la ladera del Cerro del Sol.', 'https://media-cdn.tripadvisor.com/media/photo-s/11/7e/aa/fb/jardines-del-generalife.jpg', '', 1, 1, 0, 0, 1),
+(23, 'dgp.monkeybits@gmail.com', NULL, 'Patio de los leones', 'El Patio de los Leones es el centro del Palacio que lleva este nombre. Es diferente al del Palacio de Comares, debido al eje del jardin anterior, del siglo XIII, sobre el que se disenia y a que el banio de Comares tiene un eje Norte-Sur y el palacio de los Leones tiene un eje Este-Oeste.', 'Calle Real de la Alhambra.', 'http://cadenaser00.epimg.net/emisora/imagenes/2016/08/30/radio_granada/1472561813_422996_1472561946_noticia_normal.jpg', '', 1, 1, 0, 0, 1),
+(34, 'dgp.monkeybits@gmail.com', NULL, 'Palacio de Carlos V', 'El palacio de Carlos V es una construccion renacentista situada en la colina de la Alhambra de la ciudad espaniola de Granada, en Andalucia. Desde 1958, es sede del Museo de Bellas Artes de Granada y, desde 1994, también es sede del Museo de la Alhambra.', 'calle de la Alhambra', 'https://www.101viajes.com/sites/default/files/palacio-carlos-v.jpg', 'El palacio de Carlos V es una construccion renacentista situada en la colina de la Alhambra de la ciudad espaniola de Granada.', 1, 1, 0, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -181,9 +181,7 @@ CREATE TABLE `placecomments` (
 --
 
 INSERT INTO `placecomments` (`IdPlace`, `Email`, `Content`, `Date`, `Time`, `Reported`) VALUES
-(17, 'micorreojeje@gmail.com', 'prueba', '2018-12-10', '04:00:00', 0),
-(17, 'micorreojeje@gmail.com', 'prueba 2', '2018-12-10', '11:00:00', 0),
-(17, 'usuario1@gmail.com', 'hola', '2018-12-11', '21:00:00', 1);
+(17, 'usuario1@gmail.com', 'hola', '2018-12-11', '21:00:00', 0);
 
 -- --------------------------------------------------------
 
@@ -212,12 +210,12 @@ INSERT INTO `route` (`IdRoute`, `Email`, `MadeBy`, `Name`, `Description`, `Image
 (4, 'admin1@gmail.com', 'usuariobloqueado@gmail.com', 'Ruta de prueba 3', 'Prueba', 'Vacia', ''),
 (5, 'admin1@gmail.com', 'usuario1@gmail.com', 'Ruta de prueba 4', 'Prueba', 'Vacia', ''),
 (8, 'dgp.monkeybits@gmail.com', NULL, 'Ruta de prueba 5', 'Prueba', 'Vacia', ''),
-(9, 'dgp.monkeybits@gmail.com', NULL, 'Parques y Jardines de Granada', 'Muchos jardines, florecicas, alguna que otra farola, y de vez en cuando te cae agua de las fuentes.', 'https://guiasgranada.com/advisor/wp-content/uploads/2017/06/garcia1.jpg', ''),
+(9, 'dgp.monkeybits@gmail.com', NULL, 'Parques y Jardines de Granada', 'Muchos jardines, florecicas, alguna que otra farola, y de vez en cuando te cae agua de las fuentes.', 'https://guiasgranada.com/advisor/wp-content/uploads/2017/06/garcia1.jpg', 'Muchos jardines, florecicas, alguna que otra farola, y de vez en cuando te cae agua de las fuentes.'),
 (10, '', 'usuariobloqueado@gmail.com', 'Ruta de prueba 6', 'Prueba', 'Vacia', ''),
 (11, '', 'usuario1@gmail.com', 'Ruta de prueba 7', 'Prueba', 'Vacia', ''),
 (12, '', 'usuariobloqueado@gmail.com', 'Ruta de prueba 8', 'Prueba', 'VAcia', ''),
 (16, '', 'usuario1@gmail.com', 'Ruta de prueba 9', 'prueba', 'Vacia', ''),
-(17, 'dgp.monkeybits@gmail.com', NULL, 'Tour de la Alhambra', 'Ruta por algunos de los puntos turísticos más importantes de la Alhambra, recorriendo sus grandes puertas, palaciones y jardines.', 'https://images.placesonline.com/photos/46695_granada_alcazaba.jpg?quality=80&w=710&h=510&mode=crop', '');
+(17, 'dgp.monkeybits@gmail.com', NULL, 'Tour de la Alhambra', 'Ruta por algunos de los puntos turisticos mas importantes de la Alhambra, recorriendo sus grandes puertas, palaciones y jardines.', 'https://images.placesonline.com/photos/46695_granada_alcazaba.jpg?quality=80&w=710&h=510&mode=crop', '');
 
 -- --------------------------------------------------------
 
@@ -239,11 +237,8 @@ CREATE TABLE `routecomments` (
 --
 
 INSERT INTO `routecomments` (`IdRoute`, `Email`, `Content`, `Date`, `Time`, `Reported`) VALUES
-(1, 'usuario1@gmail.com', 'Esta ruta es un timo', '2018-12-04', '15:00:00', 0),
-(3, 'usuario1@gmail.com', 'prueba', '2018-12-10', '22:00:00', 0),
-(10, 'micorreojeje@gmail.com', 'Esta ruta es un timo', '2018-12-03', '18:00:00', 1),
-(11, 'usuario1@gmail.com', 'Y esto se considera una ruta? xD\r\n', '2018-12-11', '03:00:00', 1),
-(16, 'micorreojeje@gmail.com', 'Esta ruta no me gusta', '2018-12-10', '00:10:00', 0);
+(11, 'micorreojeje@gmail.com', 'fasfeas', '2018-12-25', '02:00:00', 1),
+(16, 'usuariobloqueado@gmail.com', 'adios', '2018-12-19', '06:00:00', 0);
 
 -- --------------------------------------------------------
 
@@ -445,13 +440,13 @@ ALTER TABLE `visit`
 -- AUTO_INCREMENT de la tabla `place`
 --
 ALTER TABLE `place`
-  MODIFY `IdPlace` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `IdPlace` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT de la tabla `route`
 --
 ALTER TABLE `route`
-  MODIFY `IdRoute` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `IdRoute` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT de la tabla `suggestedplace`
@@ -474,7 +469,7 @@ ALTER TABLE `suggestedroute`
 --
 ALTER TABLE `appearsuggested`
   ADD CONSTRAINT `appearsuggested_ibfk_1` FOREIGN KEY (`IdPlace`) REFERENCES `place` (`IdPlace`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `appearsuggested_ibfk_2` FOREIGN KEY (`IdRoute`) REFERENCES `suggestedroute` (`IdRoute`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `appearsuggested_ibfk_2` FOREIGN KEY (`IdRoute`) REFERENCES `route` (`IdRoute`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `appearverified`
